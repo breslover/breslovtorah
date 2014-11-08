a14b6edba13fbed3caa5be47fb7ec04339a302ba
Poster.ly <posterly@outlook.com>
2014-11-07 20:00:19 -0600

    adding live py

---
+++ breslovtorah/live.py
@@ -0,0 +1,30 @@
+from settings import *
+
+DEBUG = False
+
+DEFAULT_FROM_EMAIL = 'no-reply@breslovtorah.net'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'mail.pawnmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
+EMAIL_HOST_PASSWORD = 'abcd4321'
+EMAIL_USE_TLS = True
+
+ALLOWED_HOSTS = ("*",)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'breslovtorah',                      
+        'USER': 'breslovtorah',
+        'PASSWORD': 'breslovtorah',
+        'HOST': 'localhost'
+    }
+}
+
+LOGGING['handlers']['console'] = {
+    'level': 'DEBUG',
+    'class': 'logging.FileHandler',
+    'filename': '/home/breslovtorah/logs/live.log',
+}
