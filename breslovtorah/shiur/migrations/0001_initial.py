f02984d334fbd5361c0aa7d56a34a7259dc4dda7
Poster.ly <posterly@outlook.com>
2014-11-09 05:30:47 -0600

    added friendly title

--- breslovtorah/shiur/migrations/0001_initial.py
+++ breslovtorah/shiur/migrations/0001_initial.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('order', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('title', models.CharField(unique=True, max_length=255)),
+                ('title_friendly', models.CharField(unique=True, max_length=255)),
                 ('slug', models.SlugField(max_length=255)),
                 ('user', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
             ],
