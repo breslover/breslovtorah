46c7874e2a3b973898ef7e203caa235e74cfae0f
Poster.ly <posterly@outlook.com>
2014-11-09 02:33:43 -0600

    admin.py!

---
+++ breslovtorah/shiur/admin.py
@@ -0,0 +1,20 @@
+'''
+Created on Nov 9, 2014
+
+@author: coderam
+'''
+
+from django.contrib import admin
+from ordered_model.admin import OrderedModelAdmin
+from models import Shiur, Sefer
+
+class ShiurAdmin(OrderedModelAdmin):
+    list_display = ('title', 'move_up_down_links')
+    list_filter = ('sefer__title',)
+    
+admin.site.register(Shiur, ShiurAdmin)
+
+class SeferAdmin(OrderedModelAdmin):
+    list_display = ('title', 'move_up_down_links')
+
+admin.site.register(Sefer, SeferAdmin)
