b7e44f4f5fa3e4227b220f979b56ac82315b9d91
Aaron Shimon <coderam@gmail.com>
2016-06-02 18:45:21 -0500

    fix

--- breslovtorah/shiur/admin.py
+++ breslovtorah/shiur/admin.py
@@ -11,10 +11,12 @@ from models import Shiur, Sefer
 class ShiurAdmin(OrderedModelAdmin):
     list_display = ('title', 'move_up_down_links')
     list_filter = ('sefer__title',)
+    exclude = ('slug',)
     
 admin.site.register(Shiur, ShiurAdmin)
 
 class SeferAdmin(OrderedModelAdmin):
     list_display = ('title', 'move_up_down_links')
+    exclude = ('slug',)
 
 admin.site.register(Sefer, SeferAdmin)
